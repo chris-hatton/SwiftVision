3e0a2fa0bf41201db8eb655b1ca48135a917d52f
Chris Hatton <christopherhattonuk@gmail.com>
2015-07-19 18:14:02 +1000

    Fresh Xcode project created

---
+++ SwiftVision/SwiftVision.h
@@ -0,0 +1,19 @@
+//
+//  SwiftVision.h
+//  SwiftVision
+//
+//  Created by Christopher Hatton on 19/07/2015.
+//  Copyright (c) 2015 Chris Hatton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftVision.
+FOUNDATION_EXPORT double SwiftVisionVersionNumber;
+
+//! Project version string for SwiftVision.
+FOUNDATION_EXPORT const unsigned char SwiftVisionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftVision/PublicHeader.h>
+
+
